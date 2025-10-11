e3c7311715db88b67840c41151a8780268875592
nconklindev <190518646+nconklindev@users.noreply.github.com>
2025-10-10 20:25:02 -0400

    add HeaderRow to FileData struct

--- internal/types/types.go
+++ internal/types/types.go
@@ -8,6 +8,7 @@ type ConversionResult struct {
 }
 
 type FileData struct {
-	Headers []string
-	Rows    [][]string
+	Headers   []string
+	Rows      [][]string
+	HeaderRow int // Which row the headers were found on in XLSX files (0-index)
 }
